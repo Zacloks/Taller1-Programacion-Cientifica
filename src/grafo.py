554d5b39c365ca6eac92f454fc77687e21201a7a
Zaikovc <miguelvalenzuela.vc@gmail.com>
2026-04-08 22:55:04 -0400

    Implementación de análisis distribución de grados
    *Se modifico la función ObtenerMayoresConexiones para que se puedan obtener tanto de conexiones de entrada como salida
    *Se agrego la función  analizarDistribuciónGrados para el análisis de grados de los nodos, tanto de entrada como salida

--- src/grafo.py
+++ src/grafo.py
@@ -15,14 +15,48 @@ class Grafo:
         origen.agregarEnlaceSalida(idDestino)
         destino.agregarEnlaceEntrada(idOrigen)
 
-    def obtenerMayoresConexiones(self, n =10):
+    def obtenerMayoresConexiones(self, n =10, tipo = "entrada"):
         lista_grados_enlaces =  []
         for nodo in self.articulos.values():
-            lista_grados_enlaces.append([nodo.gradoEntrada(), nodo])
+            
+            if tipo == "entrada":
+                valor = nodo.gradoEntrada()
+            else:
+                valor = nodo.gradoSalida()
+            lista_grados_enlaces.append([valor, nodo])
         
-        lista_grados_enlaces.sort(reverse = True)
+        lista_grados_enlaces.sort(key = lambda x: x[0], reverse = True)
         lista_resultado = []
-        
-        for grado, nodo in lista_grados_enlaces:
+
+        for grado, nodo in lista_grados_enlaces[:n]:
             lista_resultado.append(nodo)
-        return lista_resultado[:n]
+        return lista_resultado
+
+    def analizarDistribucionGrados(self, tipo = "entrada"):
+        distri = {}
+        nodos_totales = len(self.articulos)
+
+        if nodos_totales == 0:
+            return "El grafo esta vacio, no hay datos"
+        
+        for nodo in self.articulos.values():
+            if tipo == "entrada":
+                grado_actual = nodo.gradoEntrada()
+            else:
+                grado_actual = nodo.gradoSalida()
+            distri[grado_actual] = distri.get(grado_actual,0) +1
+
+        grados_ordenados = list(distri.keys())
+        grados_ordenados.sort()
+
+        print(f"---Distribución:---")
+        print(f"Cantidad de Articulos analizados: {nodos_totales}")
+        
+        for grad in grados_ordenados[:10]:
+            cantidad = distri[grad]
+            porcentaje = (cantidad / nodos_totales) * 100
+            print(f"Grado: {grad}, Cantidad articulos: {cantidad}, Porcentaje: {porcentaje:.2f}%")
+        
+        grado_maximo =  grados_ordenados[-1]
+        print(f"Grado maximo: {grado_maximo}, Cantidad articulos: {distri[grado_maximo]}")
+        return distri
